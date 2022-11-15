996ef5b7d2ee4fd0f76e8be836825d1b0a2edfa5
Chris Shiplet <chris@nearengine.com>
2022-11-14 22:15:55 -0700

    Add missing keys

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@ output "load_balancer_arn" {
 
 output "primary_listener_arn" {
   description = "ARN of the listener to attach listener rules to"
-  value       = var.certificate_arn ? aws_lb_listener.https.arn : aws_lb_listener.http.arn
+  value       = var.certificate_arn != "" ? aws_lb_listener.https[0].arn : aws_lb_listener.http[0].arn
 }
